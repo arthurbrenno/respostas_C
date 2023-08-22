0b463fdf71f16e4e22183f5baa5f3593e9ee05be
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-08-21 23:40:10 -0300

    Update questao_3.c

--- questao_3.c
+++ questao_3.c
@@ -4,7 +4,10 @@
 
 int main() {
 
+    //declarando o vetor
     int vetor[10];
+
+    //preenchendo o vetor e já verificando se ele é negativo direto depois
     for (int i = 0; i < 10; i++) {
         printf("Posicao numero %d:", i + 1);
         scanf("%d", &vetor[i]);
